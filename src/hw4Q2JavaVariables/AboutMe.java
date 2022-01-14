d3c2b6995c1c3aaed389c86a406306d542367a02
runajabbar <rnjabbr@gmail.com>
2022-01-14 00:06:05 -0500

    01142022 12:05am

---
+++ src/hw4Q2JavaVariables/AboutMe.java
@@ -0,0 +1,32 @@
+package hw4Q2JavaVariables;
+
+
+public class AboutMe {
+
+	// variables declared below
+	public String name;
+	public int houseNumber;
+	public byte age;
+	public long imaigineBalance;
+	public short mySalary;
+	public char sex;
+	public float myHeight;
+	public double myGrade;
+	public boolean nonCitizen;
+//constructor declared
+	public AboutMe() {
+
+		System.out.println("this is about me: ");
+
+	}
+//method implemented
+	
+ public void aboutMe() {
+		System.out.println("My name: " + name + "\nMy house number :"+ houseNumber + "\nMy Age : " + age +"\nBank balance:" + imaigineBalance +"\nSalary:" + mySalary + "\nSex:" + sex +"\nHeight: "+ myHeight + "\nGrade:" + myGrade + "\nImmigration Status: "+ nonCitizen);
+	}
+
+			
+	}
+	
+
+
---
+++ src/hw4Q2JavaVariables/AboutMeTest.java
@@ -0,0 +1,42 @@
+package hw4Q2JavaVariables;
+
+
+public class AboutMeTest {
+
+	public static void main(String[] args) {
+		AboutMe info = new AboutMe();//constructor initialized
+		
+		
+		info.name = "Runa Jabbar";
+		info.age = 30;
+		info.houseNumber = 455;
+		info.mySalary = 20000;
+		info.sex = 'F';
+		info.myHeight = 5.0f;
+		info.myGrade = 4.89;
+		info.nonCitizen = false;
+		info.imaigineBalance = 9000000000000l;
+		info.aboutMe();
+		
+System.out.println("\n***************************\n");
+		
+		AboutMe info2 = new AboutMe();
+		info2.name = "Alex";
+		info2.age = 33;
+		info2.houseNumber = 625;
+		info2.mySalary = 4200;
+		info2.sex = 'F';
+		info2.myHeight = 5.4f;
+		info2.myGrade = 5.90;
+		info2.nonCitizen = true;
+		info2.imaigineBalance = 80000000000l;
+		info2.aboutMe();
+		
+		
+		
+		
+	
+
+	}
+
+}
