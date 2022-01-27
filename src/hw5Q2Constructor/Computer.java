eab050dd2d462bd36288596d59696ca1b2e5697b
Runa Jabbar <rnjabbr@gmail.com>
2022-01-27 16:22:11 -0500

    01272022 4:22pm

--- src/hw5Q2Constructor/Computer.java
+++ src/hw5Q2Constructor/Computer.java
@@ -1,9 +1,5 @@
 package hw5Q2Constructor;
 
-import hw2Q2JavaBasics.MyCar;
-
-// My Brand: Apple, Model: MacBook Air, Operating system: MacOS Mojave, Price: 800$, Grade: A and Made in USA? Ans: false. I want the third outcome in console for your configuration. (Assume you are a Windows user even you are not). Organize your code. Push in the github. Paste your github link below.
-
 public class Computer {
 	public String brand;
 	public String model;
@@ -15,22 +11,15 @@ public class Computer {
 		System.out.println("This is from default constructure of Computer class.");
 	}
 
-	public Computer(String brand, String model, String operatingSystem, int price, boolean madeInUSA){
-	this.brand= brand;
-	this.model = model;
-	this.operatingSystem= operatingSystem;
-	this.price= price;
-	this.madeInUSA= madeInUSA;
-	
-	System.out.println("Computer brand:" + brand + "\nModel:" + model + "\nOperating system:" + operatingSystem + "\nPrice:" + price + "\nMade in USA:" + madeInUSA);
+	public Computer(String brand, String model, String operatingSystem, int price, boolean madeInUSA) {
+		this.brand = brand;
+		this.model = model;
+		this.operatingSystem = operatingSystem;
+		this.price = price;
+		this.madeInUSA = madeInUSA;
+
+		System.out.println("Computer brand:" + brand + "\nModel:" + model + "\nOperating system:" + operatingSystem
+				+ "\nPrice:" + price + "\nMade in USA:" + madeInUSA);
 	}
 
-
-
-		// My Brand: Apple, Model: MacBook Air, Operating system: MacOS Mojave, Price:
-		// 800$, Grade: A and Made in USA? Ans: false.
-	
-
-
-	}
-
+}
--- src/hw5Q2Constructor/ComputerTest.java
+++ src/hw5Q2Constructor/ComputerTest.java
@@ -3,11 +3,8 @@ package hw5Q2Constructor;
 public class ComputerTest {
 
 	public static void main(String[] args) {
-		
-		Computer computer= new Computer();
+		Computer computer = new Computer();
 		Computer computer2 = new Computer("Apple", "MacBook Air", "MacOS Mojave", 800, false);
-		
-		
 
 	}
 
