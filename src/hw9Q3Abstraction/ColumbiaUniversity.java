a3939aaf3f0ee1204854f99624afd1e69ffe1303
Runa Jabbar <rnjabbr@gmail.com>
2022-02-09 18:52:54 -0500

    02022022 6:52pm

--- src/hw9Q3Abstraction/ColumbiaUniversity.java
+++ src/hw9Q3Abstraction/ColumbiaUniversity.java
@@ -1,10 +1,8 @@
 package hw9Q3Abstraction;
 
 public class ColumbiaUniversity extends NYUniversity {
-	/*Keyword extends is used for the inheritance of the regular class.
-	 * Regular class can inherit other regular class or abstract class by using extends keyword but not Interface.
-	 * Regular class can inherit only one class at a time
-	 */
+	//Keyword extends is used for the inheritance of the regular class.
+	 // Regular class can inherit other regular class or abstract class by using extends keyword but not Interface.	 
 	public ColumbiaUniversity() {
 	}
 	public void biology() {
--- src/hw9Q3Abstraction/University.java
+++ src/hw9Q3Abstraction/University.java
@@ -1,21 +1,19 @@
 package hw9Q3Abstraction;
 
 public interface University extends College, Hospital {
-	/*
-	 * public University() {
-	 * }
-	 * Interface cannot create an object and it cannot declare constructor.
-	 * Object is needed to call method & interface can't create an object
-	 */
+	// public University() {
+	// }
+	// Interface cannot create an object and it cannot declare constructor.
+	// Object is needed to call method & interface can't create an object/
 	public void classSize();
 	public void playGround();
 	public void teacher();
-	 /* One keyword extends is used to for the inheritance in Interface.
-	  *Interface can inherit other Interfaces by using extends keyword, but cannot inherit regular or abstract class.
-	 * More than one Interfaces can be inherited.
-	 */
+	// One keyword extends is used to for the inheritance in Interface. Interface
+	// can inherit other Interfaces by using extends keyword, but cannot inherit
+	// regular or abstract class. More than one Interfaces can be inherited.
+
 	public default void gymnasium() {
-		}
+	}
 	public static void library() {
 	}
 }
