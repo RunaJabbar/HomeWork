eb440b806f27173537b85df294d89b54af0fda1a
Runa Jabbar <rnjabbr@gmail.com>
2022-02-10 14:24:45 -0500

    02102022 2:24pm

--- src/hw9Q3Abstraction/ColumbiaUniversity.java
+++ src/hw9Q3Abstraction/ColumbiaUniversity.java
@@ -1,10 +1,20 @@
 package hw9Q3Abstraction;
 
-public class ColumbiaUniversity extends NYUniversity {
+public class ColumbiaUniversity extends MedicalSchool {
 	//Keyword extends is used for the inheritance of the regular class.
 	 // Regular class can inherit other regular class or abstract class by using extends keyword but not Interface.	 
 	public ColumbiaUniversity() {
 	}
 	public void biology() {
+	}
+	@Override
+	public void anatomyLab() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void hygiene() {
+		// TODO Auto-generated method stub
+		
 	}
 }
