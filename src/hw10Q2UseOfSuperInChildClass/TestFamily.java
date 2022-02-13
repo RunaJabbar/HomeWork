9b99bef332351fab6449b23f33e83e0ddbe408a5
Runa Jabbar <rnjabbr@gmail.com>
2022-02-13 16:22:14 -0500

    02132022 4:22pm

--- src/hw10Q2UseOfSuperInChildClass/TestFamily.java
+++ src/hw10Q2UseOfSuperInChildClass/TestFamily.java
@@ -3,13 +3,9 @@ package hw10Q2UseOfSuperInChildClass;
 public class TestFamily {
 
 	public static void main(String[] args) {
-		Father father = new Father();
 		Daughter daughter = new Daughter();
-		
-		
-		
-		
-
+		Father father = new Father();
+		daughter.daughter();
+		father.father();
 	}
-
 }
